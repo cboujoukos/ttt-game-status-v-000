e659f615022db729e8d3410d25040f28915ccf1d
Christopher Boujoukos <cboujoukos@gmail.com>
2017-11-07 19:56:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,20 +17,24 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |winning_combo|
-    win_index_1 = winning_combo[0]
-    win_index_2 = winning_combo[1]
-    win_index_3 = winning_combo[2]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return winning_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return winning_combo
-    else
-      return false
+    winning_combo.select do |win_index|
+      board[win_index].all? == "X"
     end
+        
+    #win_index_1 = winning_combo[0]
+    #win_index_2 = winning_combo[1]
+    #win_index_3 = winning_combo[2]
+
+    #position_1 = board[win_index_1]
+    #position_2 = board[win_index_2]
+    #position_3 = board[win_index_3]
+
+    #if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      #return winning_combo
+    #elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      #return winning_combo
+    #else
+      #return false
+    #end
   end
 end
